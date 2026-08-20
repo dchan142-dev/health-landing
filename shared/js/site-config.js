1c07ac1a8e182f1ea84a57f327844e3737e7d34f
dchan142-dev <dchan142@gmail.com>
2026-08-21 02:11:42 +0800

    changed nocturia survey
    changed nocturia survey

--- shared/js/site-config.js
+++ shared/js/site-config.js
@@ -1,7 +1,7 @@
 // Health-decoder.com 全站全域參數配置
 const SITE_CONFIG = {
   MERCHANT_PHONE: "85298788564",                                // Duncan 官方 WhatsApp 號碼
-  GOOGLE_SHEET_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxShPmrQX0eGT-dmc5KfPX-cU75uE10vp-HlOILkbMRF4iz3vqsGZl2QJyYozrClLK8/exec", // 通用 Apps Script Web App URL
+  GOOGLE_SHEET_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxxxxYOUR_DEPLOY_IDxxxx/exec", // 通用 Apps Script Web App URL
   VIP_WHATSAPP_GROUP_URL: "https://chat.whatsapp.com/LPHAndy6hjH8gRLVzZacrw", // WhatsApp 私密社群
   VIP_FB_GROUP_URL: "https://www.facebook.com/share/g/1CCM9AMEyw/",          // Facebook 私密社群
   FB_PAGE_URL: "https://www.facebook.com/share/1GPoV9gghy/?mibextid=wwXIfr",  // Facebook 粉絲專頁
